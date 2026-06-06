58fbc6355dad3873baf4af8d7c6c65ef8effaca1
marcos <marcos.ayala@utec.edu.pe>
2026-06-06 17:05:55 -0500

    Phyengine updated | ParticleSys in 2d

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/marcos/Escritorio/CG/PhyEngine
-# Build directory: /home/marcos/Escritorio/CG/PhyEngine/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(freefall "/home/marcos/Escritorio/CG/PhyEngine/build/test_freefall")
-set_tests_properties(freefall PROPERTIES  _BACKTRACE_TRIPLES "/home/marcos/Escritorio/CG/PhyEngine/CMakeLists.txt;74;add_test;/home/marcos/Escritorio/CG/PhyEngine/CMakeLists.txt;0;")
